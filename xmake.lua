e0f4610c5ce3a0973eb94cde689498be7ad5ce99
jingkaimori <36156959+jingkaimori@users.noreply.github.com>
2023-04-16 13:27:21 +0800

    [xmake] add support for version information (#10)

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,14 @@
 add_requires("spdlog")
 add_requires("doctest")
+set_version("0.1.0")
 target("pscm") do
     set_kind("static")
+    add_configfiles(
+        "include/(pscm/version.h.xmake)", {
+            filename = "version.h"})
     set_languages("c++20")
     add_includedirs("include")
+    add_includedirs("$(buildir)")
     add_packages("spdlog")
     add_files("src/**.cpp")
 end
