2e71014ff004692a62cf66f02d5eb85e9df799d4
PikachuHy <pikachuhy@163.com>
2023-06-04 23:26:26 +0800

    fix ex

--- include/pscm/Exception.h
+++ include/pscm/Exception.h
@@ -14,13 +14,13 @@ namespace pscm {
 class Exception : public std::exception {
 public:
 #if defined(WASM_PLATFORM) || defined(_MSC_VER)
+  Exception(std::string msg)
+      : msg_(std::move(msg)) {
+  }
+#else
   Exception(std::string msg, ust::StackTrace stack_trace = ust::generate())
       : msg_(std::move(msg))
       , stack_trace_(std::move(stack_trace)) {
-  }
-#else
-  Exception(std::string msg)
-      : msg_(std::move(msg)) {
   }
 #endif
   const char *what() const noexcept override;
