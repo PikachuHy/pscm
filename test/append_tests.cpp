816987715b04c6d706bb7b049b996042912535e8
PikachuHy <pikachuhy@163.com>
2023-06-22 15:37:07 +0800

    c++14 compat

--- test/append_tests.cpp
+++ test/append_tests.cpp
@@ -1,7 +1,6 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
-#include <filesystem>
 #include <fstream>
 #include <pscm/Char.h>
 #include <pscm/Number.h>
@@ -17,7 +16,6 @@ using namespace doctest;
 using namespace pscm;
 using namespace std::string_literals;
 using namespace doctest;
-namespace fs = std::filesystem;
 
 TEST_CASE("testing append, 0") {
   auto f = [](Scheme& scm) {
