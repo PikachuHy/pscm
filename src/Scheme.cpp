088c3605d390fa4ab809303e13bd82b0085920cc
PikachuHy <pikachuhy@163.com>
2023-06-22 19:05:35 +0800

    fix

--- src/Scheme.cpp
+++ src/Scheme.cpp
@@ -29,7 +29,7 @@
 #include <string>
 #include <string_view>
 using namespace std::string_literals;
-#ifPSCM_STD_COMPAT
+#if PSCM_STD_COMPAT
 #include <ghc/filesystem.hpp>
 namespace fs = ghc::filesystem;
 #else
