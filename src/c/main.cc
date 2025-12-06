2ca1366b773b0106f94091359b6e1329677f866e
PikachuHy <pikachuhy@163.com>
2025-12-06 18:38:55 +0800

    Refactor SCM value initialization and type checking in main.cc
    Replace static variables for SCM singleton values with a more efficient static function approach. Introduce a helper macro for defining symbol singleton functions and a template function for type checking predicates, improving code readability and maintainability. Enhance error handling during file parsing.

--- src/c/main.cc
+++ src/c/main.cc
@@ -14,104 +14,71 @@ long *cont_base;
 
 SCM_Environment g_env;
 
-SCM *scm_none_value = nullptr;
-
-SCM *scm_nil_value = nullptr;
-
-SCM *scm_sym_let_value = nullptr;
-SCM *scm_sym_letrec_value = nullptr;
-SCM *scm_sym_set_value = nullptr;
-SCM *scm_sym_lambda_value = nullptr;
-SCM *scm_sym_quote_value = nullptr;
-
-SCM *scm_bool_true_value = nullptr;
-SCM *scm_bool_false_value = nullptr;
-
 SCM *scm_none() {
-  if (scm_none_value) {
-    return scm_none_value;
+  static SCM *value = nullptr;
+  if (!value) {
+    value = new SCM();
+    value->type = SCM::NONE;
+    value->value = nullptr;
   }
-  scm_none_value = new SCM();
-  scm_none_value->type = SCM::NONE;
-  scm_none_value->value = nullptr;
-
-  return scm_none_value;
+  return value;
 }
 
 SCM *scm_nil() {
-  if (scm_nil_value) {
-    return scm_nil_value;
+  static SCM *value = nullptr;
+  if (!value) {
+    value = new SCM();
+    value->type = SCM::NIL;
+    value->value = nullptr;
   }
-  scm_nil_value = new SCM();
-  scm_nil_value->type = SCM::NIL;
-  scm_nil_value->value = nullptr;
-  return scm_nil_value;
+  return value;
 }
 
 SCM *scm_bool_false() {
-  if (scm_bool_false_value) {
-    return scm_bool_false_value;
+  static SCM *value = nullptr;
+  if (!value) {
+    value = new SCM();
+    value->type = SCM::BOOL;
+    value->value = 0;
   }
-  scm_bool_false_value = new SCM();
-  scm_bool_false_value->type = SCM::BOOL;
-  scm_bool_false_value->value = 0;
-  return scm_bool_false_value;
+  return value;
 }
 
 SCM *scm_bool_true() {
-  if (scm_bool_true_value) {
-    return scm_bool_true_value;
+  static SCM *value = nullptr;
+  if (!value) {
+    value = new SCM();
+    value->type = SCM::BOOL;
+    value->value = (void *)1;
   }
-  scm_bool_true_value = new SCM();
-  scm_bool_true_value->type = SCM::BOOL;
-  scm_bool_true_value->value = (void *)1;
-  return scm_bool_true_value;
+  return value;
 }
 
 SCM_Symbol *make_sym(const char *data) {
   auto sym = new SCM_Symbol();
   size_t len = strlen(data);
-  sym->data = (char *)malloc(len + 1);
+  sym->data = new char[len + 1];
   memcpy(sym->data, data, len);
   sym->data[len] = '\0';  // Ensure null terminator
   sym->len = len;
   return sym;
 }
 
-SCM *scm_sym_let() {
-  if (scm_sym_let_value) {
-    return scm_sym_let_value;
+// Helper macro to define symbol singleton functions
+#define DEFINE_SYMBOL_SINGLETON(func_name, symbol_name) \
+  SCM *scm_sym_##func_name() { \
+    static SCM *value = nullptr; \
+    if (!value) { \
+      value = wrap(make_sym(symbol_name)); \
+    } \
+    return value; \
   }
-  return wrap(make_sym("let"));
-}
 
-SCM *scm_sym_quote() {
-  if (scm_sym_quote_value) {
-    return scm_sym_quote_value;
-  }
-  return wrap(make_sym("quote"));
-}
-
-SCM *scm_sym_letrec() {
-  if (scm_sym_letrec_value) {
-    return scm_sym_letrec_value;
-  }
-  return wrap(make_sym("letrec"));
-}
-
-SCM *scm_sym_set() {
-  if (scm_sym_set_value) {
-    return scm_sym_set_value;
-  }
-  return wrap(make_sym("set!"));
-}
-
-SCM *scm_sym_lambda() {
-  if (scm_sym_lambda_value) {
-    return scm_sym_lambda_value;
-  }
-  return wrap(make_sym("lambda"));
-}
+DEFINE_SYMBOL_SINGLETON(let, "let")
+DEFINE_SYMBOL_SINGLETON(quote, "quote")
+DEFINE_SYMBOL_SINGLETON(letrec, "letrec")
+DEFINE_SYMBOL_SINGLETON(set, "set!")
+DEFINE_SYMBOL_SINGLETON(lambda, "lambda")
 
 SCM *create_sym(const char *data, int len) {
   SCM_Symbol *sym = new SCM_Symbol();
@@ -133,36 +100,27 @@ SCM *eval(SCM *ast) {
   return ret;
 }
 
-SCM *scm_c_is_procedure(SCM *arg) {
+// Helper function for type checking predicates
+template <typename Predicate>
+SCM *scm_c_type_check(SCM *arg, Predicate pred) {
   assert(arg);
-  if (is_proc(arg) || is_cont(arg) || is_func(arg)) {
-    return scm_bool_true();
-  }
-  return scm_bool_false();
+  return pred(arg) ? scm_bool_true() : scm_bool_false();
+}
+
+SCM *scm_c_is_procedure(SCM *arg) {
+  return scm_c_type_check(arg, [](SCM *a) { return is_proc(a) || is_cont(a) || is_func(a); });
 }
 
 SCM *scm_c_is_boolean(SCM *arg) {
-  assert(arg);
-  if (is_bool(arg)) {
-    return scm_bool_true();
-  }
-  return scm_bool_false();
+  return scm_c_type_check(arg, is_bool);
 }
 
 SCM *scm_c_is_null(SCM *arg) {
-  assert(arg);
-  if (is_nil(arg)) {
-    return scm_bool_true();
-  }
-  return scm_bool_false();
+  return scm_c_type_check(arg, is_nil);
 }
 
 SCM *scm_c_is_pair(SCM *arg) {
-  assert(arg);
-  if (is_pair(arg)) {
-    return scm_bool_true();
-  }
-  return scm_bool_false();
+  return scm_c_type_check(arg, is_pair);
 }
 
 void init_scm() {
@@ -251,8 +209,12 @@ please report bugs to https://github.com/PikachuHy/pscm/issues
 }
 
 int do_eval(const char *filename) {
-  auto l = parse_file(filename);
   init_scm();
+  auto l = parse_file(filename);
+  if (!l) {
+    fprintf(stderr, "ERROR: failed to parse file: %s\n", filename);
+    return 1;
+  }
   while (l) {
     auto expr = l->data;
     my_eval(expr);
